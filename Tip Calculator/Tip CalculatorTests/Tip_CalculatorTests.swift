d57ca40a894b05ca430a7e669246678923becf33
Amani <144798976+Kamani05@users.noreply.github.com>
2024-11-09 20:10:09 -0500

    Add files via upload

---
+++ Tip Calculator/Tip CalculatorTests/Tip_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorTests.swift
+//  Tip CalculatorTests
+//
+//  Created by Amani Ngomasele on 11/8/24.
+//
+
+import Testing
+@testable import Tip_Calculator
+
+struct Tip_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
